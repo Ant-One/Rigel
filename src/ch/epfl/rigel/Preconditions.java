ddb7192642d3edf0ae11104425b2e13fe6f1fa8a
Antoine <antoine.moix@gmail.com>
2020-02-17 14:03:17 +0100

    Created Preconditions.java

--- src/ch/epfl/rigel/Preconditions.java
+++ src/ch/epfl/rigel/Preconditions.java
@@ -1,4 +1,30 @@
+/**
+ * Utilitary Class used to check things
+ *
+ * @author Antoine Moix (310052)
+ */
+
 package ch.epfl.rigel;
 
-public class Preconditions {
+public final class Preconditions {
+
+    /**
+     * Useless constructor
+     */
+    private Preconditions() {}
+
+    /**
+     * Method that throws an IllegalArgumentException if its parameter is false
+     * @param isTrue parameter to use
+     *
+     * @throws IllegalArgumentException if isTrue is false
+     */
+    void checkArgument(boolean isTrue) throws IllegalArgumentException{
+        if(!isTrue){
+            throw new IllegalArgumentException();
+        }
+
+    }
+    double checkInInterval(Interval interval, double value){ //TODO Class Interval
+    }
 }
