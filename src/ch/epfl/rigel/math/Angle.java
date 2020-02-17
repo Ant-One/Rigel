f2dcdeeef568e4eb5bd64bec212f0dade77383ba
Antoine <antoine.moix@gmail.com>
2020-02-17 14:32:39 +0100

    Added doc

--- src/ch/epfl/rigel/math/Angle.java
+++ src/ch/epfl/rigel/math/Angle.java
@@ -1,5 +1,9 @@
 package ch.epfl.rigel.math;
 
+/**
+ * Utilitary class for angles
+ * @author Antoine Moix (310052)
+ */
 public final class Angle {
 
     public final static double TAU = Math.PI * 2.0; //We define here the Tau constant
@@ -19,8 +23,13 @@ public final class Angle {
         return 0;
     }
 
+    /**
+     * Return the value in degrees for a given radian angle
+     * @param rad angle in radians to convert
+     * @return the given angle in degrees
+     */
     double toDeg(double rad){
-        return 0;
+        return Math.toDegrees(rad);
     }
 
     double ofHr(double hr){
