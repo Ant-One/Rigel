b17670e8ae3aca340c24323a542c44dba6033dd6
Antoine <antoine.moix@gmail.com>
2020-02-17 14:28:11 +0100

    Created Angle.java and stub methods

---
+++ src/ch/epfl/rigel/math/Angle.java
@@ -0,0 +1,34 @@
+package ch.epfl.rigel.math;
+
+public final class Angle {
+
+    public final static double TAU = Math.PI * 2.0; //We define here the Tau constant
+
+    private Angle(){
+    }
+
+    double normalizePositive(double rad){
+        return 0;
+    }
+
+    double ofArcsec(double sec){
+        return 0;
+    }
+
+    double ofDMS(int deg, int min, double sec){
+        return 0;
+    }
+
+    double toDeg(double rad){
+        return 0;
+    }
+
+    double ofHr(double hr){
+        return 0;
+    }
+
+    double toHr(double rad){
+        return 0;
+    }
+
+}
