35da1da52a6d1a561ebb5246708b10bd8071f49d
Antoine <antoine.moix@gmail.com>
2020-02-19 14:25:22 +0100

    Added methods stubs

--- src/ch/epfl/rigel/coordinates/HorizontalCoordinates.java
+++ src/ch/epfl/rigel/coordinates/HorizontalCoordinates.java
@@ -1,8 +1,46 @@
 package ch.epfl.rigel.coordinates;
 
+
+/**
+ * Class modelling horizontal coordinates
+ *
+ * @author Antoine Moix (310052)
+ */
  public final class HorizontalCoordinates extends SphericalCoordinates{
 
      private HorizontalCoordinates(){
+
+     }
+
+     public static HorizontalCoordinates of(double az, double alt){
+
+     }
+
+     public static HorizontalCoordinates ofDeg(double azDeg, double altDeg){
+
+     }
+
+     public double az(){
+
+     }
+
+     public double azDeg(){
+
+     }
+
+     public String azOctantName(String n, String e, String s, String w){
+
+     }
+
+     public double alt(){
+
+     }
+
+     public double altDeg(){
+
+     }
+
+     public double angularDistanceTo(HorizontalCoordinates that){
 
      }
 }
