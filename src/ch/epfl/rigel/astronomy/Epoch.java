d9b47f5f5b3c5d69158431a16e62b2e925a70fed
Antoine <antoine.moix@gmail.com>
2020-02-25 11:41:07 +0100

    Commented Epoch

--- src/ch/epfl/rigel/astronomy/Epoch.java
+++ src/ch/epfl/rigel/astronomy/Epoch.java
@@ -18,17 +18,29 @@ public enum Epoch {
             ZoneOffset.UTC));
 
     private ZonedDateTime beginning;
-    private static double DAYS_PER_MILLIS = (double) 1/(1000 * 60 * 60 * 24);
+    private static double DAYS_PER_MILLIS = (double) 1/(ChronoUnit.DAYS.getDuration().toMillis());
     private static double CENTURIES_PER_MILLIS = (double) 1/ChronoUnit.CENTURIES.getDuration().toMillis();
 
     Epoch(ZonedDateTime beginning){
         this.beginning = beginning;
     }
 
+    /**
+     * Returns the number of days (not necesserly an integer) between the beginning date of the standard (J2000 or J2010)
+     * and the when parameter
+     * @param when date to which compute the numbers of days remaining
+     * @return a non-integer number of days remaining
+     */
     public double daysUntil(ZonedDateTime when) {
         return beginning.until(when, ChronoUnit.MILLIS) * DAYS_PER_MILLIS;
     }
 
+    /**
+     * Returns the number of julian centuries (not necesserly an integer) between the beginning date of the standard (J2000 or J2010)
+     * and the when parameter
+     * @param when date to which compute the numbers of julian centuries remaining
+     * @return a non-integer number of julian centuries remaining
+     */
     public double julianCenturiesUntil(ZonedDateTime when){
         return beginning.until(when, ChronoUnit.MILLIS) * CENTURIES_PER_MILLIS;
     }
