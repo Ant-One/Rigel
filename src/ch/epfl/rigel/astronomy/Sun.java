bd16850eb0accfa60cf1bf13dfa5420c64d803da
Antoine <antoine.moix@gmail.com>
2020-03-02 14:48:35 +0100

    Began Sun

---
+++ src/ch/epfl/rigel/astronomy/Sun.java
@@ -0,0 +1,16 @@
+package ch.epfl.rigel.astronomy;
+
+import ch.epfl.rigel.coordinates.EclipticCoordinates;
+import ch.epfl.rigel.coordinates.EquatorialCoordinates;
+
+/**
+ * Class modelling the Sun
+ *
+ * @author Antoine Moix (310052)
+ */
+public final class Sun extends CelestialObject{
+
+    public Sun(EclipticCoordinates eclipticPos, EquatorialCoordinates equatorialPos, float angularSize, float meanAnomaly){
+        super();
+    }
+}
