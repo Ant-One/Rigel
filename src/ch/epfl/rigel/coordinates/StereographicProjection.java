f470dca84c5fd34689ac4ec44afe9c15bab49a36
Adrinels <adrien.rey@epfl.ch>
2020-03-02 13:46:41 +0100

    Cartesian

--- src/ch/epfl/rigel/coordinates/StereographicProjection.java
+++ src/ch/epfl/rigel/coordinates/StereographicProjection.java
@@ -9,4 +9,57 @@ public class StereographicProjection implements Function<HorizontalCoordinates, 
     public StereographicProjection(HorizontalCoordinates center) {
         this.center = center;
     }
+
+    public CartesianCoordinates circleCenterForParallel(HorizontalCoordinates hor){
+
+        return CartesianCoordinates.of(0,0);
+
+
+
+
+
+
+
+
+    }
+
+    double circleRadiusForParallel(HorizontalCoordinates parallel){
+        return 0;
+    }
+
+    double applyToAngle(double rad){
+        return 0;
+    }
+
+    public CartesianCoordinates apply(HorizontalCoordinates azAlt){
+        return null;
+    }
+
+
+    public HorizontalCoordinates inverseApply(CartesianCoordinates xy){
+        return null;
+    }
+
+    /**
+     * @throws UnsupportedOperationException when used
+     */
+    @Override
+    final public int hashCode() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * @throws UnsupportedOperationException when used
+     */
+    @Override
+    public boolean equals(Object obj) {
+        throw new UnsupportedOperationException();
+    }
+
+
+
+
+
+
+
 }
