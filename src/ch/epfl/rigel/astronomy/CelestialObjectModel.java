7831b2f180017895e9a529af2b00fcbf3548882f
Antoine <antoine.moix@gmail.com>
2020-03-11 15:04:12 +0100

    Annoted CelestialObjectModel

--- src/ch/epfl/rigel/astronomy/CelestialObjectModel.java
+++ src/ch/epfl/rigel/astronomy/CelestialObjectModel.java
@@ -2,7 +2,17 @@ package ch.epfl.rigel.astronomy;
 
 import ch.epfl.rigel.coordinates.EclipticToEquatorialConversion;
 
+/**
+ * @author Antoine Moix (310052)
+ * @param <O> the type of the model
+ */
 public interface CelestialObjectModel<O> {
 
+    /**
+     * Method to compute the object after a number of days (can be negative!)
+     * @param daysSinceJ2010 number of days after J2010
+     * @param eclipticToEquatorialConversion conversion to convert ecliptic coordinates to equatorial ones
+     * @return the object at the time specified
+     */
     public abstract O at(double daysSinceJ2010, EclipticToEquatorialConversion eclipticToEquatorialConversion);
 }
