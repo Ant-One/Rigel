876ad757f98e2f7c6742ac144b1272f7063cc7d8
Antoine <antoine.moix@gmail.com>
2020-03-20 12:38:17 +0100

    Added StarCatalogue

---
+++ src/ch/epfl/rigel/astronomy/StarCatalogue.java
@@ -0,0 +1,28 @@
+package ch.epfl.rigel.astronomy;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Catalogue of stars
+ *
+ * @author Antoine Moix (310052)
+ */
+public final class StarCatalogue {
+
+    StarCatalogue(List<Star> stars, List<Asterism> asterisms){
+
+    }
+
+    List<Star> stars(){
+
+    }
+
+    Set<Asterism> asterisms(){
+
+    }
+
+    List<Integer> asterismIndices(Asterism asterism){
+
+    }
+}
