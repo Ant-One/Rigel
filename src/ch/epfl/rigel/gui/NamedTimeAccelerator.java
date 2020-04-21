028619b3e848d2a30a837ab8430ee506366ec4d4
Adrinels <adrien.rey@epfl.ch>
2020-04-21 16:03:33 +0200

    TimeAccelerator continious

---
+++ src/ch/epfl/rigel/gui/NamedTimeAccelerator.java
@@ -0,0 +1,50 @@
+package ch.epfl.rigel.gui;
+
+import java.time.Duration;
+
+public enum NamedTimeAccelerator {
+    TIMES_1("1x",TimeAccelerator.continuous(1)),
+    TIMES_30("30x",TimeAccelerator.continuous(30)),
+    TIMES_300("300x",TimeAccelerator.continuous(300)),
+    TIMES_3000("3000x",TimeAccelerator.continuous(3000)),
+    DAY("jour",TimeAccelerator.discrete(60, Duration.ofHours(24))),
+    SIDEREAL_DAY("jour sidéral",TimeAccelerator.discrete(60,Duration.ofSeconds((23*60+56)*60+4)));
+
+    private final String name;
+    private final TimeAccelerator accelerator;
+
+    /**
+     * Construct a Named Accelerator
+     * @param name the name
+     * @param accelerator the accelerator
+     */
+    NamedTimeAccelerator(String name, TimeAccelerator accelerator) {
+        this.name = name;
+        this.accelerator = accelerator;
+    }
+
+    /**
+     * the name
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * the TimeAccelerator
+     * @return the accelerator
+     */
+    public TimeAccelerator getAccelerator() {
+        return accelerator;
+    }
+
+    /**
+     * Method toString
+     * @return the name of the accelerator
+     */
+    @Override
+    public String toString() {
+        return name;
+    }
+}
