3d2447c271449ad91a725180812ea44b377d7465
Adrinels <adrien.rey@epfl.ch>
2020-04-21 15:45:05 +0200

    TimeAccelerator continious

--- src/ch/epfl/rigel/gui/TimeAccelerator.java
+++ src/ch/epfl/rigel/gui/TimeAccelerator.java
@@ -1,20 +1,35 @@
 package ch.epfl.rigel.gui;
 
+import java.time.Duration;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 
 @FunctionalInterface
 public interface TimeAccelerator {
-
+    /** abstract fonction of an accelerator
+     * @param T0 Initial time
+     * @param deltaTime spent time since the last tick
+     * @return the adjusted time
+     */
     ZonedDateTime adjust(ZonedDateTime T0, long deltaTime);
 
+    /**
+     * Create a continuous accelerator
+     * @param alpha the acceleration factor
+     * @return the accelerator
+     */
     static TimeAccelerator continuous(int alpha){
         return ((T0, deltaTime) -> T0.plus(alpha*deltaTime, ChronoUnit.NANOS));
     }
 
-    static  TimeAccelerator discrete(){
-        return null;
+    /**
+     * Create a discrete accelerator
+     * @param v the advancement frequencies
+     * @param S the step
+     * @return the accelerator
+     */
+    static  TimeAccelerator discrete(int v, Duration S){
+        return((T0,deltaTime) -> T0.plus((long)Math.floor(v*deltaTime)*S.toNanos(),ChronoUnit.NANOS));
     }
-
 
 }
