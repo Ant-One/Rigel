8700fd444d8e7a5e96ea176542c61e380b9c2d78
Adrinels <adrien.rey@epfl.ch>
2020-05-05 14:23:56 +0200

    debut du main

--- src/ch/epfl/rigel/gui/Main.java
+++ src/ch/epfl/rigel/gui/Main.java
@@ -2,9 +2,9 @@ package ch.epfl.rigel.gui;
 
 import javafx.application.Application;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-
-import java.awt.*;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -16,10 +16,29 @@ public class Main extends Application {
 
         BorderPane root=new BorderPane();
 
+        root.setTop(controlBarre());
+        root.setCenter(sky());
+        root.setBottom(infoBarre());
+
 
         stage.setMinHeight(600);
         stage.setMinWidth(800);
         stage.setTitle("Rigel");
 
     }
+
+    private HBox controlBarre(){
+
+        return null;
+    }
+
+    private Pane sky(){
+    return null;
+    }
+
+    private BorderPane infoBarre(){
+        return null;
+    }
+
+
 }
