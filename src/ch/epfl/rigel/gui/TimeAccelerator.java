186b1269a78c8a5dd79bcabcecf05fc19bd3276f
Antoine <antoine.moix@gmail.com>
2020-05-12 17:06:18 +0200

    Cleaned TimeAccelerator

--- src/ch/epfl/rigel/gui/TimeAccelerator.java
+++ src/ch/epfl/rigel/gui/TimeAccelerator.java
@@ -6,12 +6,16 @@ import java.time.temporal.ChronoUnit;
 
 /**
  * Models a time accelerator
+ *
  * @author Adrien Rey (313388)
  */
 @FunctionalInterface
 public interface TimeAccelerator {
-    /** abstract function of an accelerator
-     * @param T0 Initial time
+
+    /**
+     * abstract function of an accelerator
+     *
+     * @param T0        Initial time
      * @param deltaTime spent time since the last tick
      * @return the adjusted time
      */
@@ -19,21 +23,23 @@ public interface TimeAccelerator {
 
     /**
      * Create a continuous accelerator
+     *
      * @param alpha the acceleration factor
      * @return the accelerator
      */
-    static TimeAccelerator continuous(int alpha){
-        return ((T0, deltaTime) -> T0.plus(alpha*deltaTime, ChronoUnit.NANOS));
+    static TimeAccelerator continuous(int alpha) {
+        return ((T0, deltaTime) -> T0.plus(alpha * deltaTime, ChronoUnit.NANOS));
     }
 
     /**
      * Create a discrete accelerator
+     *
      * @param v the advancement frequencies
      * @param S the step§
      * @return the accelerator
      */
-    static  TimeAccelerator discrete(int v, Duration S){
-        return((T0,deltaTime) -> T0.plus((long)Math.floor(v*deltaTime*1e-9)*S.toSeconds(),ChronoUnit.SECONDS));
+    static TimeAccelerator discrete(int v, Duration S) {
+        return ((T0, deltaTime) -> T0.plus((long) Math.floor(v * deltaTime * 1e-9) * S.toSeconds(), ChronoUnit.SECONDS));
     }
 
 }
