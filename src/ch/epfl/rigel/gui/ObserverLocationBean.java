7e21387215de36149b7e907c4436628bc1c3293c
Antoine <antoine.moix@gmail.com>
2020-05-13 17:38:15 +0200

    Changed ObjectProperty to DoubleProperty

--- src/ch/epfl/rigel/gui/ObserverLocationBean.java
+++ src/ch/epfl/rigel/gui/ObserverLocationBean.java
@@ -2,7 +2,9 @@ package ch.epfl.rigel.gui;
 
 import ch.epfl.rigel.coordinates.GeographicCoordinates;
 import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.value.ObservableObjectValue;
 
@@ -13,8 +15,8 @@ import javafx.beans.value.ObservableObjectValue;
  */
 public class ObserverLocationBean {
 
-    private ObjectProperty<Double> lonDeg ;
-    private ObjectProperty<Double> latDeg;
+    private DoubleProperty lonDeg ;
+    private DoubleProperty latDeg;
 
     private ObjectProperty<GeographicCoordinates> coordinates;
     private ObservableObjectValue<GeographicCoordinates> coordinatesBindings;
@@ -23,8 +25,8 @@ public class ObserverLocationBean {
      * Basic constructor of the bean
      */
     public ObserverLocationBean() {
-        lonDeg = new SimpleObjectProperty<>(0d);
-        latDeg = new SimpleObjectProperty<>(0d);
+        lonDeg = new SimpleDoubleProperty(0d);
+        latDeg = new SimpleDoubleProperty(0d);
         coordinates = new SimpleObjectProperty<>(null);
         coordinatesBindings = Bindings.createObjectBinding(
                 () -> GeographicCoordinates.ofDeg(lonDeg.get(), latDeg.get()),
@@ -37,7 +39,7 @@ public class ObserverLocationBean {
      *
      * @return the longitude property
      */
-    public ObjectProperty<Double> lonDegProperty() {
+    public DoubleProperty lonDegProperty() {
         return lonDeg;
     }
 
@@ -65,7 +67,7 @@ public class ObserverLocationBean {
      *
      * @return the latitude property
      */
-    public ObjectProperty<Double> latDegProperty() {
+    public DoubleProperty latDegProperty() {
         return latDeg;
     }
 
