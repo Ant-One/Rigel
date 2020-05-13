7ca5695277105544cd27541a94ca9fb8b8101be1
Antoine <antoine.moix@gmail.com>
2020-05-13 17:36:10 +0200

    Reformated code

--- src/ch/epfl/rigel/gui/ObserverLocationBean.java
+++ src/ch/epfl/rigel/gui/ObserverLocationBean.java
@@ -8,77 +8,109 @@ import javafx.beans.value.ObservableObjectValue;
 
 /**
  * JavaFX Bean of the location of the observer
+ *
  * @author Adrien Rey (313388)
  */
 public class ObserverLocationBean {
 
-    private ObjectProperty<Double> lonDeg=null;
-    private  ObjectProperty<Double> latDeg=null;;
-    private ObjectProperty<GeographicCoordinates> coordinates=null;
+    private ObjectProperty<Double> lonDeg ;
+    private ObjectProperty<Double> latDeg;
+
+    private ObjectProperty<GeographicCoordinates> coordinates;
     private ObservableObjectValue<GeographicCoordinates> coordinatesBindings;
 
     /**
      * Basic constructor of the bean
      */
     public ObserverLocationBean() {
-        lonDeg=new SimpleObjectProperty<>(0d);
-        latDeg=new SimpleObjectProperty<>(0d);
-        coordinates=new SimpleObjectProperty<>();
+        lonDeg = new SimpleObjectProperty<>(0d);
+        latDeg = new SimpleObjectProperty<>(0d);
+        coordinates = new SimpleObjectProperty<>(null);
         coordinatesBindings = Bindings.createObjectBinding(
-                () ->GeographicCoordinates.ofDeg(lonDeg.get(),latDeg.get()),
-                lonDeg,latDeg);
+                () -> GeographicCoordinates.ofDeg(lonDeg.get(), latDeg.get()),
+                lonDeg, latDeg);
         coordinatesBindings.addListener(o -> coordinates.set(coordinatesBindings.get()));
     }
 
     /**
      * return the longitude property
+     *
      * @return the longitude property
      */
-    public ObjectProperty<Double> lonDegProperty(){return lonDeg;}
+    public ObjectProperty<Double> lonDegProperty() {
+        return lonDeg;
+    }
 
     /**
      * return the longitude in degree
+     *
      * @return the longitude in degree
      */
-    public double getLonDeg(){return lonDeg.get();}
+    public double getLonDeg() {
+        return lonDeg.get();
+    }
 
     /**
      * change the longitude
+     *
      * @param lonDeg the new longitude in degree
      */
-    public void setLonDeg(double lonDeg){this.lonDeg.set(lonDeg); }
+    public void setLonDeg(double lonDeg) {
+        this.lonDeg.set(lonDeg);
+    }
 
 
     /**
      * return the latitude property
+     *
      * @return the latitude property
      */
-    public ObjectProperty<Double> latDegProperty(){return latDeg;}
+    public ObjectProperty<Double> latDegProperty() {
+        return latDeg;
+    }
+
     /**
      * return the latitude in degree
+     *
      * @return the latitude in degree
      */
-    public double getLatDeg(){return latDeg.get();}
+    public double getLatDeg() {
+        return latDeg.get();
+    }
+
     /**
      * change the latitude
+     *
      * @param latDeg the new latitude in degree
      */
-    public void setLatDeg(double latDeg){this.latDeg.set(latDeg); }
+    public void setLatDeg(double latDeg) {
+        this.latDeg.set(latDeg);
+    }
 
     /**
      * return the geographic coordinates property
+     *
      * @return the geographic coordinates property
      */
-    public ObjectProperty<GeographicCoordinates> coordinatesBindingsProperty(){return coordinates;}
+    public ObjectProperty<GeographicCoordinates> coordinatesBindingsProperty() {
+        return coordinates;
+    }
+
     /**
      * return the geographic coordinates
+     *
      * @return the geographic coordinates
      */
-    public GeographicCoordinates getCoordinates(){return coordinates.get();}
+    public GeographicCoordinates getCoordinates() {
+        return coordinates.get();
+    }
 
     /**
      * change the geographic coordinates
+     *
      * @param coordinates the new geographic coordinates
      */
-    public void setCoordinates(GeographicCoordinates coordinates){this.coordinates.set(coordinates);}
+    public void setCoordinates(GeographicCoordinates coordinates) {
+        this.coordinates.set(coordinates);
+    }
 }
